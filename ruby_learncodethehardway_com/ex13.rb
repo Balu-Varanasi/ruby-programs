6e76e06f5a6fd84cfc54229415c826c5d18307b7
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-25 17:11:03 +0530

    added ex13.rb

--- ruby_learncodethehardway_com/ex13.rb
+++ ruby_learncodethehardway_com/ex13.rb
@@ -1,8 +1,13 @@
 first, second, third = ARGV
 
+puts "The script is called: #{$0}"
+puts
 puts "ARGV is an %s" % ARGV.class
 
-puts "The script is called: #{$0}"
+puts "first argument is of class - %s" % first.class
+puts "second argument is of class #{second.class}"
+puts "third argument is of class %s" % third.class
+
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
