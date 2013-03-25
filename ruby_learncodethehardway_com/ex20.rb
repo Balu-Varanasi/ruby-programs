cbba84079c731e75968e38ed93a64930d762fe9a
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-26 01:04:02 +0530

    changed ex20.rb

--- ruby_learncodethehardway_com/ex20.rb
+++ ruby_learncodethehardway_com/ex20.rb
@@ -8,6 +8,10 @@ def rewind(f)
   f.seek(0, IO::SEEK_SET)
 end
 
+def rewind_seek_end(f)
+  f.seek(0, IO::SEEK_END)
+end
+
 def print_a_line(line_count, f)
   puts "#{line_count} #{f.readline()}"
 end
@@ -33,3 +37,11 @@ print_a_line(current_line, current_file)
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+
+puts "rewind_seek_end"
+rewind_seek_end(current_file)
+
+current_line = 1
+# ex20.rb:16:in `readline': end of file reached (EOFError)
+# the following line gives an error
+#print_a_line(current_line, current_file) 
